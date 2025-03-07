2327ef7287042ac8a006e397290d4d1ae143408a
Oleh Liubichenko <oleh.liubichenko@gmail.com>
2025-03-07 04:22:07 +0200

    Add snake animation

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Ultra Snake Game 2000")
+screen.tracer(0)
 
 segments = []
 
 for i in range(3):
-    s = Turtle()
-    s.shape("square")
-    s.color("white")
-    segments.append(s)
+    seg = Turtle()
+    seg.shape("square")
+    seg.color("white")
+    seg.penup()
+    seg.goto(x=(i*-20),y=0)
+    segments.append(seg)
 
-segments[1].goto(x=-20,y=0)
-segments[2].goto(x=-40,y=0)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
